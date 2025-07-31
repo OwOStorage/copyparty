62675f127bae0aa198b078a4eb35b1e0f77ef0ee
ed <s@ocv.me>
2025-07-31 18:19:47 +0000

    tl cleanup

--- copyparty/web/splash.js
+++ copyparty/web/splash.js
@@ -98,33 +98,33 @@ var Ls = {
 		"a1": "Neu laden",
 		"b1": "Tach, wie geht's? &nbsp; <small>(Du bist nicht angemeldet)</small>",
 		"c1": "Abmelden",
-		"d1": "Zustand",  // TLNote: "d2" is the tooltip for this button
+		"d1": "Zustand",
 		"d2": "Zeigt den Zustand aller aktiven Threads",
 		"e1": "Config neu laden",
 		"e2": "Konfigurationsdatei neu laden (Accounts/Volumes/VolFlags)$Nund scannt alle e2ds-Volumes$N$NBeachte: Jegliche Änderung an globalen Einstellungen$Nbenötigt einen Neustart zum Anwenden",
 		"f1": "Du kannst lesen:",
 		"g1": "Du kannst hochladen nach:",
 		"cc1": "Andere Dinge:",
-		"h1": "k304 deaktivieren",  // TLNote: "j1" explains what k304 is
+		"h1": "k304 deaktivieren",
 		"i1": "k304 aktivieren",
 		"j1": "k304 trennt die Clientverbindung bei jedem HTTP 304, was Bugs mit problematischen Proxies vorbeugen kann (z.B. nicht ladenden Seiten), macht Dinge aber generell langsamer",
 		"k1": "Client-Einstellungen zurücksetzen",
 		"l1": "Melde dich an für mehr:",
-		"m1": "Willkommen zurück,",  // TLNote: "welcome back, USERNAME"
+		"m1": "Willkommen zurück,",
 		"n1": "404 Nicht gefunden &nbsp;┐( ´ -`)┌",
 		"o1": 'or maybe you don\'t have access -- try a password or <a href="' + SR + '/?h">go home</a>',
 		"p1": "403 Verboten &nbsp;~┻━┻",
 		"q1": 'Benutze ein Passwort oder <a href="' + SR + '/?h">gehe zur Homepage</a>',
 		"r1": "Gehe zur Homepage",
 		".s1": "Neu scannen",
-		"t1": "Aktion",  // TLNote: this is the header above the "rescan" buttons
+		"t1": "Aktion",
 		"u2": "time since the last server write$N( upload / rename / ... )$N$N17d = 17 days$N1h23 = 1 hour 23 minutes$N4m56 = 4 minutes 56 seconds",
 		"v1": "Verbinden",
 		"v2": "Benutze diesen Server als lokale Festplatte",
 		"w1": "Zu HTTPS wechseln",
 		"x1": "Passwort ändern",
-		"y1": "Shares bearbeiten",  // TLNote: shows the list of folders that the user has decided to share
-		"z1": "Share entsperren:",  // TLNote: the password prompt to see a hidden share
+		"y1": "Shares bearbeiten",
+		"z1": "Share entsperren:",
 		"ta1": "Trage zuerst dein Passwort ein",
 		"ta2": "Wiederhole dein Passwort zur Bestätigung:",
 		"ta3": "Da stimmt etwas nicht; probier's nochmal",
@@ -133,39 +133,40 @@ var Ls = {
 		"ac1": "no304 aktivieren",
 		"ad1": "Das Aktivieren von no304 deaktiviert jegliche Form von Caching; probier dies, wenn k304 nicht genug war. Dies verschwendet eine grosse Menge Netzwerk-Traffic!",
 		"ae1": "Aktive Downloads:",
-        "af1": "Zeige neue Uploads",
+		"af1": "Zeige neue Uploads",
 	},
-	"eng": {
+
+	"rus": {
 		"a1": "обновить",
 		"b1": "приветик, незнакомец &nbsp; <small>(вы не авторизованы)</small>",
 		"c1": "выйти",
-		"d1": "трассировка стека",  // TLNote: "d2" is the tooltip for this button
+		"d1": "трассировка стека",
 		"d2": "показывает состояние всех активных потоков",
 		"e1": "перезагрузить конфиг",
 		"e2": "перезагрузить файлы конфига (аккаунты/хранилища/флаги),$Nи пересканировать все хранилища с флагом e2ds$N$Nвнимание: изменения глобальных настроек$Nтребуют полного перезапуска сервера",
 		"f1": "вы можете видеть:",
 		"g1": "вы можете загружать файлы в:",
 		"cc1": "всякая всячина:",
-		"h1": "отключить k304",  // TLNote: "j1" explains what k304 is
+		"h1": "отключить k304",
 		"i1": "включить k304",
 		"j1": "включённый k304 будет отключать вас при получении HTTP 304, что может помочь при работе с некоторыми глючными прокси (перестают загружаться страницы), <em>но</em> это также сделает работу клиента медленнее",
 		"k1": "сбросить локальные настройки",
 		"l1": "авторизуйтесь для других опций:",
-		"m1": "с возвращением,",  // TLNote: "welcome back, USERNAME"
+		"m1": "с возвращением,",
 		"n1": "404 не найдено &nbsp;┐( ´ -`)┌",
 		"o1": 'или у вас нет доступа -- попробуйте авторизоваться или <a href="' + SR + '/?h">вернуться на главную</a>',
 		"p1": "403 доступ запрещён &nbsp;~┻━┻",
 		"q1": 'авторизуйтесь или <a href="' + SR + '/?h">вернитесь на главную</a>',
 		"r1": "вернуться на главную",
 		".s1": "пересканировать",
-		"t1": "действия",  // TLNote: this is the header above the "rescan" buttons
+		"t1": "действия",
 		"u2": "время с последней записи на сервер$N( загрузка / переименование / ... )$N$N17d = 17 дней$N1h23 = 1 час 23 минут$N4m56 = 4 минут 56 секунд",
 		"v1": "подключить",
 		"v2": "использовать сервер как локальный диск",
 		"w1": "перейти на https",
 		"x1": "поменять пароль",
-		"y1": "управление доступом",  // TLNote: shows the list of folders that the user has decided to share
-		"z1": "разблокировать:",  // TLNote: the password prompt to see a hidden share
+		"y1": "управление доступом",
+		"z1": "разблокировать:",
 		"ta1": "сначала введите свой новый пароль",
 		"ta2": "повторите новый пароль:",
 		"ta3": "опечатка; попробуйте снова",
